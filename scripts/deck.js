16604221918755e9eb7bb322071c4c1663e47892
Mashego Sililo <isthmussa@gmail.com>
2021-05-11 20:09:48 +0200

    SCSS styling & Consolidation of pages

--- scripts/deck.js
+++ scripts/deck.js
@@ -1,3 +1,33 @@
+// Animation for responsive nav bar
+const hamburger = document.querySelector(".hamburger");
+const links = document.querySelector(".links");
+const allLinks = document.querySelectorAll(".links .link-style");
+
+hamburger.addEventListener("click", () => {
+  links.classList.toggle("open");
+});
+
+// Tabs
+function openTab(evt, tabName) {
+  var i, suite, tabLink;
+  
+  suite = document.getElementsByClassName("suite");
+  for (i=0; i< suite.length; i++) {
+      suite[i].style.display = 'none';
+  }
+
+  tabLink = document.getElementsByClassName("tabLink");
+  for (i=0; i <tabLink.length; i++) {
+      tabLink[i].className = tabLink[i].className.replace("active", "");
+   
+  }
+ 
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += "active";
+}
+
+
 
 // Page Animations
 const sequence = gsap.timeline({defaults: {ease: "power1.out"} });
@@ -7,14 +37,6 @@ sequence.to('.slider', {y:"-100%", duration: 1.5, delay: 0.8});
 
 sequence.to('.animation', {y:"-100%", duration: 0.5}, "-=1");
 
-// Animation for responsive nav bar
-const hamburger = document.querySelector(".hamburger");
-const links = document.querySelector(".links");
-const allLinks = document.querySelectorAll(".links .link-style");
-
-hamburger.addEventListener("click", () => {
-  links.classList.toggle("open");
-});
 
 // flip card functionality
 function flip(event) {
