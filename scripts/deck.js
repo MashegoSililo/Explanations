538763e7962005b02257617bff2bfe2f9f601cf5
Mashego Sililo <isthmussa@gmail.com>
2021-08-30 17:11:16 +0200

    Modal functionality

--- scripts/deck.js
+++ scripts/deck.js
@@ -7,36 +7,24 @@ hamburger.addEventListener("click", () => {
   links.classList.toggle("open");
 });
 
-// Resource Modals
-var modal = document.getElementById("openResource");
 
-const button = document.getElementById("button");
+// // closing the modal
 
-var closeModal = document.getElementsByClassName("close")[0];
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
 
-button.onclick = function() {
-  modal.style.display = "block";
-}
 
-// closing the modal
-closeModal.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target === modal) {
-    modal.style.display = "none"
-  }
-}
 
 // Page Animations
-const sequence = gsap.timeline({defaults: {ease: "power1.out"} });
+const sequence = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-sequence.to('.a-text', {y:"0%", duration: 0.5});
-sequence.to('.slider', {y:"-100%", duration: 1.5, delay: 0.8});
+sequence.to(".a-text", { y: "0%", duration: 0.5 });
+sequence.to(".slider", { y: "-100%", duration: 1.5, delay: 0.8 });
 
-sequence.to('.animation', {y:"-100%", duration: 0.5}, "-=1");
-
+sequence.to(".animation", { y: "-100%", duration: 0.5 }, "-=1");
 
 // flip card functionality
 function flip(event) {
