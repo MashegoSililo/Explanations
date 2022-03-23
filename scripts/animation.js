af9f08e1da5961fd64234c625e5dc1604e88cc6e
Mashego Sililo <isthmussa@gmail.com>
2022-03-23 21:04:25 +0200

    Changes to index structure and style

---
+++ scripts/animation.js
@@ -0,0 +1,7 @@
+const subheading = document.querySelector(".subheading");
+
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    subheading.classList.add("show");
+  }, 500);
+}, 2000);
